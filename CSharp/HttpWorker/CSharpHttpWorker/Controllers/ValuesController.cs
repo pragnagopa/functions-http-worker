c5c578b232c0afe12c09e5ad17a4002732e2b5be
pragnagopa <pgopa@microsoft.com>
2019-08-09 07:05:01 -0700

    Add go

--- CSharp/HttpWorker/CSharpHttpWorker/Controllers/ValuesController.cs
+++ CSharp/HttpWorker/CSharpHttpWorker/Controllers/ValuesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace CSharpHttpWorker.Controllers
 {
@@ -10,11 +11,18 @@ namespace CSharpHttpWorker.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        private ILogger<ValuesController> _logger = null;
+        public ValuesController(ILogger<ValuesController> logger)
+        {
+            _logger = logger;
+        }
         // GET api/values
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
             var invocationRequest = this.HttpContext;
+            _logger.LogInformation($"scriptFile:{invocationRequest.Request.Headers["ScriptFile"]}");
+            _logger.LogInformation($"myQueueItem:{invocationRequest.Request.Headers["myQueueItem"]}");
             return new string[] { "value1", "value2" };
         }
 
---
+++ go/src/httpServer/helloworldServer.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+
+func main() {
+    http.HandleFunc("/invokeFunction", handler)
+    log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "%s %s %s \n", r.Method, r.URL, r.Proto)
+    //Iterate over all header fields
+    for k, v := range r.Header {
+        fmt.Fprintf(w, "Header field %q, Value %q\n", k, v)
+    }
+
+    fmt.Fprintf(w, "Host = %q\n", r.Host)
+    fmt.Fprintf(w, "RemoteAddr= %q\n", r.RemoteAddr)
+    //Get value for a specified token
+    fmt.Fprintf(w, "\n\nFinding value of \"myQueueItem\" %q", r.Header["myQueueItem"])
+    w.Write([]byte("HTTP status code returned!"))
+}
