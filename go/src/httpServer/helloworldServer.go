62e786bc764871fd9f0b9d182ed13c6163c0064d
pragnagopa <pgopa@microsoft.com>
2019-08-15 14:54:56 -0700

    Handler per function

--- go/src/httpServer/helloworldServer.go
+++ go/src/httpServer/helloworldServer.go
@@ -6,13 +6,8 @@ import (
     "net/http"
 )
 
-func main() {
-    http.HandleFunc("/invokeFunction", handler)
-    log.Fatal(http.ListenAndServe("localhost:8000", nil))
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-    //fmt.Fprintf(w, "%s %s %s \n", r.Method, r.URL, r.Proto)
+func queueTriggerHandler(w http.ResponseWriter, r *http.Request) {
+     //fmt.Fprintf(w, "%s %s %s \n", r.Method, r.URL, r.Proto)
     //Iterate over all header fields
     for k, v := range r.Header {
         fmt.Fprintf(w, "Header field %q, Value %q\n", k, v)
@@ -23,4 +18,18 @@ func handler(w http.ResponseWriter, r *http.Request) {
     //Get value for a specified token
     fmt.Fprintf(w, "Go HttpServer of Myqueueitem", r.Header["Myqueueitem"])
     w.Write([]byte("HTTP status code returned!"))
+  }
+  
+  func httpTriggerHandler(w http.ResponseWriter, r *http.Request) {
+    queryParamName := r.URL.Query().Get("name")
+	w.Write([]byte("Hello World from go worker:" + queryParamName))
+  }
+
+func main() {
+    mux := http.NewServeMux()
+
+    mux.HandleFunc("/invoke/httpTriggerHandler", httpTriggerHandler)
+    mux.HandleFunc("/invoke/queueTriggerHandler", queueTriggerHandler)
+    log.Println("Go server Listening...")
+    log.Fatal(http.ListenAndServe(":8000", mux))
 }
