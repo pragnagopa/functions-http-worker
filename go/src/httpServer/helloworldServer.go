b4fc80f8093e6fa31fc555bf9038f6a8ebcb7d2f
pragnagopa <pgopa@microsoft.com>
2019-09-24 11:53:55 -0700

    send json response

--- go/src/httpServer/helloworldServer.go
+++ go/src/httpServer/helloworldServer.go
@@ -1,43 +1,49 @@
 package main
-
 import (
     "fmt"
     "log"
     "net/http"
+    "time"
+    "encoding/json"
 )
-
+type Profile struct {
+    Name    string
+    Hobbies []string
+  }
 func queueTriggerHandler(w http.ResponseWriter, r *http.Request) {
-     //fmt.Fprintf(w, "%s %s %s \n", r.Method, r.URL, r.Proto)
-    //Iterate over all header fields
     for k, v := range r.Header {
         fmt.Fprintf(w, "Header field %q, Value %q\n", k, v)
     }
-
-    //fmt.Fprintf(w, "Host = %q\n", r.Host)
-    //fmt.Fprintf(w, "RemoteAddr= %q\n", r.RemoteAddr)
-    //Get value for a specified token
-    //fmt.Fprintf(w, "Go HttpServer of Myqueueitem", r.Header["Myqueueitem"])
     queueItemArray := r.Header["Myqueueitem"]
     queueItem := ""
     for _, element := range queueItemArray {
-        // index is the index where we are
-        // element is the element from someSlice for where we are
         queueItem = queueItem + element
     }
     w.Write([]byte("Go server returning Myqueueitem: " + queueItem))
   }
   
   func httpTriggerHandler(w http.ResponseWriter, r *http.Request) {
-    queryParamName := r.URL.Query().Get("name")
-    
-	w.Write([]byte("Hello World from go worker:" + queryParamName))
+    t := time.Now()
+    fmt.Println(t.Month())
+    fmt.Println(t.Day())
+    fmt.Println(t.Year())
+    //w.Write([]byte("Hello World from go worker:pgopa"))
+    profile := Profile{"Alex", []string{"snowboarding", "programming"}}
+
+  js, err := json.Marshal(profile)
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    return
+  }
+
+  w.Header().Set("Content-Type", "application/json")
+  w.Write(js)
   }
 
 func main() {
     mux := http.NewServeMux()
-
-    mux.HandleFunc("/invoke/httpTriggerHandler", httpTriggerHandler)
-    mux.HandleFunc("/invoke/queueTriggerHandler", queueTriggerHandler)
+    mux.HandleFunc("/HttpTrigger", httpTriggerHandler)
+    mux.HandleFunc("/QueueTrigger", queueTriggerHandler)
     log.Println("Go server Listening...")
-    log.Fatal(http.ListenAndServe(":8000", mux))
+    log.Fatal(http.ListenAndServe(":8090", mux))
 }
