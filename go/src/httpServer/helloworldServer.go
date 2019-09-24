0531e9b8dbef7d08af5021ffcd5dac86e5bd8882
pragnagopa <pgopa@microsoft.com>
2019-09-24 15:26:38 -0700

    send invocationresponse

--- go/src/httpServer/helloworldServer.go
+++ go/src/httpServer/helloworldServer.go
@@ -6,21 +6,53 @@ import (
     "time"
     "encoding/json"
 )
-type Profile struct {
-    Name    string
-    Hobbies []string
-  }
+type ReturnValue struct {
+    Data string
+}
+type InvokeResponse struct {
+    Outputs  map[string]interface{}
+	Logs []string
+	ReturnValue
+}
+
+type InvokeRequest struct {
+    Data  map[string]interface{}
+    Metadata  map[string]interface{}
+}
+
 func queueTriggerHandler(w http.ResponseWriter, r *http.Request) {
-    for k, v := range r.Header {
-        fmt.Fprintf(w, "Header field %q, Value %q\n", k, v)
+    var invokeReq InvokeRequest
+    d := json.NewDecoder(r.Body)
+    decodeErr := d.Decode(&invokeReq)
+    if decodeErr != nil {
+    // bad JSON or unrecognized json field
+    http.Error(w, decodeErr.Error(), http.StatusBadRequest)
+    return
     }
-    queueItemArray := r.Header["Myqueueitem"]
-    queueItem := ""
-    for _, element := range queueItemArray {
-        queueItem = queueItem + element
+    fmt.Println("The JSON data is:invokeReq metadata......")
+    fmt.Println(invokeReq.Metadata)
+    fmt.Println("The JSON data is:invokeReq data......")
+    fmt.Println(invokeReq.Data)
+    
+
+   returnValue := ReturnValue{Data:"HelloWorld"}
+	outputs := make(map[string]interface{})
+    outputs["output"] = "output from go"
+    outputs["output2"] = map[string]interface{}{
+    "home": "123-466-799",
+    "office": "564-987-654",
     }
-    w.Write([]byte("Go server returning Myqueueitem: " + queueItem))
+    invokeResponse := InvokeResponse{outputs, []string{"test log1", "test log2"},returnValue}
+
+  js, err := json.Marshal(invokeResponse)
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    return
   }
+
+  w.Header().Set("Content-Type", "application/json")
+  w.Write(js)
+}
   
   func httpTriggerHandler(w http.ResponseWriter, r *http.Request) {
     t := time.Now()
@@ -28,9 +60,16 @@ func queueTriggerHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Println(t.Day())
     fmt.Println(t.Year())
     //w.Write([]byte("Hello World from go worker:pgopa"))
-    profile := Profile{"Alex", []string{"snowboarding", "programming"}}
+    returnValue := ReturnValue{Data:"return val"}
+	outputs := make(map[string]interface{})
+    outputs["output"] = "Mark Taylor"
+    outputs["output2"] = map[string]interface{}{
+    "home": "123-466-799",
+    "office": "564-987-654",
+    }
+    invokeResponse := InvokeResponse{outputs, []string{"test log1", "test log2"},returnValue}
 
-  js, err := json.Marshal(profile)
+  js, err := json.Marshal(invokeResponse)
   if err != nil {
     http.Error(w, err.Error(), http.StatusInternalServerError)
     return
