718ca3de96f636971c0fe7a524856858e61ed73d
pragnagopa <pgopa@microsoft.com>
2020-01-30 09:17:49 -0800

    HttpTrigger Json response

--- go/src/httpServer/helloworldServer.go
+++ go/src/httpServer/helloworldServer.go
@@ -17,6 +17,12 @@ type InvokeResponse struct {
 	ReturnValue interface{}
 }
 
+type InvokeResponseStringReturnValue struct {
+  Outputs  map[string]interface{}
+	Logs []string
+	ReturnValue string
+}
+
 type InvokeRequest struct {
     Data  map[string]interface{}
     Metadata  map[string]interface{}
@@ -137,6 +143,43 @@ w.Write(js)
   w.Write(js)
   }
 
+  func httpTriggerHandlerStringReturnValue(w http.ResponseWriter, r *http.Request) {
+    t := time.Now()
+    fmt.Println(t.Month())
+    fmt.Println(t.Day())
+    fmt.Println(t.Year())
+    ua := r.Header.Get("User-Agent")
+    fmt.Printf("user agent is: %s \n", ua)
+    invocationid := r.Header.Get("X-Azure-Functions-InvocationId")
+    fmt.Printf("invocationid is: %s \n", invocationid)
+    
+	outputs := make(map[string]interface{})
+    outputs["output"] = "Mark Taylor"
+    outputs["output2"] = map[string]interface{}{
+    "home": "123-466-799",
+    "office": "564-987-654",
+    }
+    headers := make(map[string]interface{})
+    headers["header1"] = "header1Val"
+    headers["header2"] = "header2Val"
+
+    res := make(map[string]interface{})
+    res["statusCode"] = "201"
+    res["body"] = "my world"
+    res["headers"]=headers
+    outputs["res"] =res
+    invokeResponse := InvokeResponseStringReturnValue{outputs, []string{"test log1", "test log2"},"Hello,World"}
+
+  js, err := json.Marshal(invokeResponse)
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    return
+  }
+
+  w.Header().Set("Content-Type", "application/json")
+  w.Write(js)
+  }
+
   func simpleHttpTriggerHandler(w http.ResponseWriter, r *http.Request) {
     t := time.Now()
     fmt.Println(t.Month())
@@ -163,6 +206,7 @@ func main() {
     }
     mux := http.NewServeMux()
     mux.HandleFunc("/HttpTrigger", httpTriggerHandler)
+    mux.HandleFunc("/HttpTriggerStringReturnValue", httpTriggerHandlerStringReturnValue)
     mux.HandleFunc("/QueueTrigger", queueTriggerHandler)
     mux.HandleFunc("/BlobTrigger", blobTriggerHandler)
     mux.HandleFunc("/QueueTriggerWithOutputs", queueTriggerWithOutputsHandler)
